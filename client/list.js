ce6821ce91a30fd8d5c8d3f9b75fd98fedde3ef9
Jodu555 <Jodu505@gmail.com>
2021-06-25 22:42:27 +0200

    Added that the client reloads the table and dismisses the modal if submitted

--- client/list.js
+++ client/list.js
@@ -1,9 +1,11 @@
 const addHosterForm = document.getElementById('addHosterForm');
 
-addHosterForm.addEventListener('submit', (event) => {
+loadHosters();
+
+addHosterForm.addEventListener('submit', async (event) => {
 	event.preventDefault();
 	var data = new FormData(addHosterForm);
-	fetch(API_URL + 'hoster/create', {
+	await fetch(API_URL + 'hoster/create', {
 		method: 'POST',
 		headers: {
 			'auth-token': 'DEV-TOKEN-SECRET',
@@ -11,71 +13,72 @@ addHosterForm.addEventListener('submit', (event) => {
 		},
 		body: JSON.stringify(formDataToObject(data)),
 	});
+    $('#editModal').modal('hide');
+    var myModal = new bootstrap.Modal(document.getElementById('editModal'), {
+        keyboard: false
+    })
+    myModal.hide()
+	loadHosters();
 });
 
-function formDataToObject(formData) {
-    const obj = {};
-    for (var key of formData.keys()) {
-        obj[key] = formData.get(key);
-	}
-    return obj;
-}
-
-loadHosters();
-
-function deleteHoster(uuid) {
-    fetch(API_URL + 'hoster/delete/' + uuid, {
+async function deleteHoster(uuid) {
+	await fetch(API_URL + 'hoster/delete/' + uuid, {
 		method: 'GET',
 		headers: {
 			'auth-token': 'DEV-TOKEN-SECRET',
 		},
 	});
+	loadHosters();
 }
 
 async function loadHosters() {
-    const response = await fetch(API_URL + 'hoster/list', {
+	const response = await fetch(API_URL + 'hoster/list', {
 		method: 'GET',
 		headers: {
 			'auth-token': 'DEV-TOKEN-SECRET',
 		},
 	});
-    const json = await response.json();
-    let i = 0;
-    json.forEach(hoster => {
-        i++;
-        hoster.id = i;
-        hoster.name = `<a href="hoster.html?uuid=${hoster.UUID}">${hoster.name}</a>`
-        hoster.services = -1;
-        hoster.delete = `<button class="btn btn-danger" onclick="deleteHoster('${hoster.UUID}')">Delete</button>`
-    });
-    loadTable(json)
+	const json = await response.json();
+	let i = 0;
+	json.forEach((hoster) => {
+		i++;
+		hoster.id = i;
+		hoster.name = `<a href="hoster.html?uuid=${hoster.UUID}">${hoster.name}</a>`;
+		hoster.services = -1;
+		hoster.delete = `<button class="btn btn-danger" onclick="deleteHoster('${hoster.UUID}')">Delete</button>`;
+	});
+	await loadTable(json);
 }
+
+let initial = true;
 
 function loadTable(data) {
-    $('#table').bootstrapTable({
-        search: true,
-        columns: [
-            {
-                sortable: true,
-                field: 'id',
-                title: 'ID',
-            },
-            {
-                field: 'name',
-                title: 'Name',
-            },
-            {
-                sortable: true,
-                field: 'services',
-                title: 'Services',
-            },
-            {
-                field: 'delete',
-                title: 'Delete',
-            },
-        ],
-        data,
-    });
+	$table = $('#table');
+
+	$table.bootstrapTable('destroy');
+
+	$table.bootstrapTable({
+		search: true,
+		columns: [
+			{
+				sortable: true,
+				field: 'id',
+				title: 'ID',
+			},
+			{
+				field: 'name',
+				title: 'Name',
+			},
+			{
+				sortable: true,
+				field: 'services',
+				title: 'Services',
+			},
+			{
+				field: 'delete',
+				title: 'Delete',
+			},
+		],
+		data,
+	});
 }
-
-
