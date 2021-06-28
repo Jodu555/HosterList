17fe6357c0528d91e429da954dfb8ca38af330c7
Jodu555 <Jodu505@gmail.com>
2021-06-28 02:10:38 +0200

    Added setup.sql

--- setup.sql
+++ setup.sql
@@ -1,16 +1,4 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server Version:               10.4.11-MariaDB - mariadb.org binary distribution
--- Server Betriebssystem:        Win64
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Exportiere Datenbank Struktur für hosterlist
